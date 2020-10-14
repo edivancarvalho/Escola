2147c15df0c32eb2922f2f829f37b6d18764d15d
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-14 01:48:05 -0300

    Update AlunosBll.java
    Contrutor recebendo a conexão atravez da alunoDao pelo objeto AlunosDao;
    
    adicionar metodo que receber os parametros da classe Alunos → aluno.

--- ProjetoEscola/src/bll/AlunosBll.java
+++ ProjetoEscola/src/bll/AlunosBll.java
@@ -1,9 +1,27 @@
 package bll;
 
+import dao.AlunosDao;
+import model.Alunos;
+
 /**
  *
  * @author edivan
  */
 public class AlunosBll {
+
+    // chama a classe AlunoDao;
+    private AlunosDao alunoDao;
+
+    //Contrutor recebendo a conexão atravez da alunoDao pelo objeto AlunosDao;
+    public AlunosBll() {
+        alunoDao = new AlunosDao();
+    }
+
+    // metodo adicionar receber os parametros da classe Alunos → aluno.
+    public void adicionarAlunos(Alunos aluno) throws Exception {
+        // 
+        alunoDao.adicionarAlunos(aluno);
+        
+    }
     
 }
