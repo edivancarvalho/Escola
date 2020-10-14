9e54d5a3582627190929f9ab543c611723e8c90f
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-14 01:47:01 -0300

    Update Alunos.java
    Adicionando os get-set e o 2 construutores. O padrão e com 2 paramentros.

--- ProjetoEscola/src/model/Alunos.java
+++ ProjetoEscola/src/model/Alunos.java
@@ -5,5 +5,33 @@ package model;
  * @author edivan
  */
 public class Alunos {
+    private int id;
+    private String nome;
+
+    public Alunos() {
+    }
+
+    public Alunos(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
     
 }
